171a03a87c21086a5b1acda507d3ae6496e795a1
David <benoid@Davids-MacBook-Pro.local>
2017-05-18 19:30:58 -0400

    kotlin

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &&
 
 $(cd ~/.vim/bundle && \
   git clone https://github.com/wlangstroth/vim-racket.git && \
-  git clone https://github.com/flazz/vim-colorschemes.git)
+  git clone https://github.com/flazz/vim-colorschemes.git && \
+  git clone https://github.com/udalov/kotlin-vim ~/.vim/bundle/kotlin-vim)
+
 
 echo "execute pathogen#infect()" > ~/.vimrc
 echo "syntax enable" >> ~/.vimrc
