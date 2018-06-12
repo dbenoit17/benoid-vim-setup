f2d6d6267325798a4bed47060740263166c9dec0
dbenoit17 <dbenoit@redhat.com>
2018-06-12 10:20:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,17 +7,16 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &&
 
 $(cd ~/.vim/bundle && \
   git clone https://github.com/wlangstroth/vim-racket.git && \
-  git clone https://github.com/flazz/vim-colorschemes.git && \
-  git clone https://github.com/udalov/kotlin-vim ~/.vim/bundle/kotlin-vim)
+  git clone https://github.com/flazz/vim-colorschemes.git)
 
-
-echo "execute pathogen#infect()" > ~/.vimrc
-echo "syntax enable" >> ~/.vimrc
-echo "filetype plugin indent on" >> ~/.vimrc
-echo "set expandtab" >> ~/.vimrc
-echo "set number" >> ~/.vimrc
-echo "colorscheme alduin" >> ~/.vimrc
-
+cat > ~/.vimrc << END
+execute pathogen#infect()
+syntax enable
+filetype plugin indent off
+set expandtab
+set number
+colorscheme alduin
+END
 
 
 
